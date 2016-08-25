6e00045d5d68ed371996dd0e740c1d731ff7c8da
Said <smoya@ekoomedia.co>
2016-08-24 20:26:35 -0500

    =Carga inicial con taller 2

---
+++ Taller2/Taller2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Taller2
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			new StringFinder();
+			Console.Read();
+		}
+	}
+}
---
+++ Taller2/Taller2/StringFinder.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace Taller2
+{
+	public class StringFinder
+	{
+		public StringFinder()
+		{
+			Console.Write("Escriba la cadena general: ");
+			char[] cadenaOrigien = (Console.ReadLine()).ToCharArray();
+			Console.Write("Escriba la cadena a buscar: ");
+			char[] cadenaBusqueda = (Console.ReadLine()).ToCharArray();
+
+			int n = cadenaOrigien.Length,
+				m = cadenaBusqueda.Length;
+
+			int pMatch = -1;
+			for (int i = 0; i <= (n-m); i++)
+			{
+				bool match = true;
+				for (int j = 0; j < m; j++)
+				{
+					if (cadenaOrigien[i+j] != cadenaBusqueda[j])
+					{
+						match = false;
+						break;
+					}
+				}
+
+				if (match)
+				{
+					pMatch = i;
+					break;
+				}
+			}
+			if (pMatch != -1){
+				Console.WriteLine("Palabra encontrada en la posición " + pMatch);
+			}
+			else {
+				Console.WriteLine("Palabra no encontrada");
+			}
+		}
+	}
+}
+
